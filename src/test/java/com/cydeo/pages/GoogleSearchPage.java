6d6c5968a85dfaaa2af0719b8d3a95d5b9f6a3bb
Muhammet <muhammedabuchary@gmail.com>
2022-03-18 10:25:14 -0500

    Day3_Cucumber_GoogleStepDefinitions

--- src/test/java/com/cydeo/pages/GoogleSearchPage.java
+++ src/test/java/com/cydeo/pages/GoogleSearchPage.java
@@ -1,4 +1,17 @@
 package com.cydeo.pages;
 
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class GoogleSearchPage {
+
+    public GoogleSearchPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    //we can start locating web elements using @FindBy annotation
+    @FindBy(name ="q") // "name q" always for Google search box
+    public WebElement searchBox;
 }
--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -1,20 +1,32 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.GoogleSearchPage;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.Keys;
 
 public class GoogleStepDefinitions {
 
+    GoogleSearchPage googleSearchPage = new GoogleSearchPage();
+    //object from GoogleSearchPage class to call searchBox method
+
     @When("User types apple and click enter")
     public void user_types_apple_and_click_enter() {
-
+        googleSearchPage.searchBox.sendKeys("apple"+ Keys.ENTER);
+        //called method from GoogleSearchPage with finding Google search box
+        // putting there our search word and push enter
     }
+
     @Then("User sees apple in the google title")
     public void user_sees_apple_in_the_google_title() {
-
+        String expectedTitle = "apple - Google Search";
+        String actualTitle = Driver.getDriver().getTitle();
+        //verify if the title is as expected after finding 'apple'
+        Assert.assertEquals("Title is not as expected", expectedTitle, actualTitle);
+        //a bit another order than in TestNG
     }
 
     @When("User in on Google search page")
@@ -26,7 +38,7 @@ public class GoogleStepDefinitions {
     public void user_should_see_title_is_google() {
         String expectedTitle = "Google";
         String actualTitle = Driver.getDriver().getTitle();
-        Assert.assertEquals(actualTitle,expectedTitle);
+        Assert.assertEquals(actualTitle, expectedTitle);
 
         Driver.closeDriver();
     }
