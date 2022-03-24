888a081de48454b4b95751002f01308b333018ea
Muhammet <muhammedabuchary@gmail.com>
2022-03-24 11:17:56 -0500

    Day6_Cucumber_CukesRunner_newOperations

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -7,6 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
+               // "pretty",
+                //'pretty' just print out additional info about scenario is executed
                 "html:target/cucumber-report.html",
                 //multiple time of reports, 'html' in this case,
                 //'target' is our folder in project, then we name the 'html' report
@@ -17,7 +19,7 @@ import org.junit.runner.RunWith;
                 //'rerun' will build .txt report if something is failed in test with
                 // indication a path where this error is /lines number in feature file/
                 //file:src/test/resources/features/webtableOrder.feature:37:39/ in this case
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
                 //new from pom.xml dependency, create target/cucumber automatically
                 //you can open this report in browser separately by clicking:
                 // target/cucumber/cucumber-html-reports/overview-features.html
@@ -30,7 +32,7 @@ import org.junit.runner.RunWith;
         //dryRun = true -> do not run my step_definitions(do not run browser)
                            // but only check if any step is missing snippet
         //dryRun = false -> turn off dryRun and our code will run as expected
-        tags =  "@smoke"               //"wip" -> wip means "work in progress"
+        tags =  "@smoke" ,              //"wip" -> wip means "work in progress"
                 //"@Regression"//named it by custom, as you want, only make sense where you place it
         //can be @smoke, @librarian, @student or @admin which are in login.feature
         //you can even "@librarian or @student" will execute 2 scenarios
@@ -40,7 +42,9 @@ import org.junit.runner.RunWith;
         //@Regression is placed at the top of login.feature to execute whole code
         //if we place @something at the top only one Scenario of login.feature
         // then it will execute only this Scenario
-
+        publish = false
+        //'publish' will generate report automatically a public link in a console
+        //  you can open it in browser or share this link
 )
 
 public class CukesRunner {
